bd76642415dd4e72f3cc54e7c303fc3396a8a776
gimli <lucas@homeloc.com>
2013-04-23 14:39:57 +0900

    Removing useless abstract keyword in TFE interface

--- supersql/codegenerator/TFE.java
+++ supersql/codegenerator/TFE.java
@@ -4,30 +4,30 @@ package supersql.codegenerator;
 
 import supersql.extendclass.ExtList;
 
-public abstract interface TFE {
-	public abstract void debugout(int count);
+public interface TFE {
+	public void debugout(int count);
 
-	public abstract ExtList makesch();
+	public ExtList makesch();
 
-	public abstract ExtList makele0();
+	public ExtList makele0();
 
-	public abstract void work(ExtList data_info);
+	public void work(ExtList data_info);
 
-	public abstract int countconnectitem();
+	public int countconnectitem();
 
-	public abstract void addDeco(String key, Object val);
+	public void addDeco(String key, Object val);
 
-	public abstract void setId(int id);
-	public abstract int getId();
-	
+	public void setId(int id);
+	public int getId();
+
 	//hanki start
-	public abstract void setOrderBy(String order);
-	public abstract void setAggregate(String aggregate);
+	public void setOrderBy(String order);
+	public void setAggregate(String aggregate);
 	//hanki end
-	
+
 	//added by ria 20110913 start
-	public abstract ExtList makeschImage();
+	public ExtList makeschImage();
 	//added by ria 20110913 end
 
-	public abstract void addDeco(String name, String value, String condition);
+	public void addDeco(String name, String value, String condition);
 }
