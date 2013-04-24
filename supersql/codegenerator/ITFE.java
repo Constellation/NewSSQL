69acef995cb3d27c4dec35f701cdf52aca38a0c2
gimli <lucas@homeloc.com>
2013-04-24 23:27:20 +0900

    Moving interface to ITFE, removing redundant modifiers

---
+++ supersql/codegenerator/ITFE.java
@@ -0,0 +1,33 @@
+/* TFE abstract class */
+
+package supersql.codegenerator;
+
+import supersql.extendclass.ExtList;
+
+public interface ITFE {
+	public void debugout(int count);
+
+	ExtList<TFE> makesch();
+
+	ExtList makele0();
+
+	void work(ExtList data_info);
+
+	int countconnectitem();
+
+	void addDeco(String key, Object val);
+
+	void setId(int id);
+	int getId();
+
+	//hanki start
+	void setOrderBy(String order);
+	void setAggregate(String aggregate);
+	//hanki end
+
+	//added by ria 20110913 start
+	ExtList makeschImage();
+	//added by ria 20110913 end
+
+	void addDeco(String name, String value, String condition);
+}
