6156f93b52f740f416dfce48aa20c854b430d851
gimli <lucas@homeloc.com>
2013-05-09 11:05:52 +0900

    merge stashed commit

--- supersql/parser/SSQLparser.java
+++ supersql/parser/SSQLparser.java
@@ -59,8 +59,6 @@ public class SSQLparser {
 	private String groupStatement;
 	private String havingStatement;
 	private String media;
-	private String orderStatement;
-
 	private String QueryImage;
 
 	private int tableNum = 0;
@@ -313,7 +311,6 @@ public class SSQLparser {
 	}
 
 	private void processFROM(StringBuffer tfe, StringTokenizer st) {
-
 		while (st.hasMoreTokens()) {
 			String nt = st.nextToken().toString();
 			if (nt.equalsIgnoreCase("FROM"))
@@ -548,6 +545,12 @@ public class SSQLparser {
 		}
 	}
 
+	/**
+	 * Fills the buffers from_c, where_c, order_c, group_c and having_c.
+	 * Whenever we encounter a keyword the local variable buffer changes
+	 * and points to the next buffer.
+	 * @param st
+	 */
 	private void processKeywords(StringTokenizer st){
 		StringBuffer buffer = new StringBuffer();
 		buffer = from_c;
@@ -620,7 +623,6 @@ public class SSQLparser {
 		}
 
 		QueryImage = QueryString;
-
 		StringTokenizer st = new StringTokenizer(QueryString);
 
 		try {
@@ -680,7 +682,6 @@ public class SSQLparser {
 			return;
 		}
 	}
-	
 
 	private void preProcess(StringTokenizer st, String nt) {
 		// FOREACH
