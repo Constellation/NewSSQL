ca124074d5ea9e4ca730d52ea2867deb318fc9b3
Goto <goto@db.ics.keio.ac.jp>
2014-01-22 06:30:23 +0900

    Changed the check process of embedded string. 2

--- supersql/parser/Embed.java
+++ supersql/parser/Embed.java
@@ -6,51 +6,63 @@ import supersql.codegenerator.DecorateList;
 public class Embed {
 	
 	final static String EMBED_LABEL = "_embedLabel_";
+	public static boolean embed = false;
 	
 	public static String checkEmbed(String q) {
-		
+		//Log.e("q="+q);
 		while(q.contains("<$")){
-			//Log.e("c!! q="+q);
 			int index1,index2;
 			index1 = q.indexOf("<$");
 			String buf1 = q.substring(index1+2);
 			index2 = buf1.indexOf("$>")+4;
 			String buf2 = buf1.substring(0, index2-4);
 			
-			buf2 = buf2.replace("\"", "\\\"");	//TODO check
-			
-			q = q.substring(0,index1) + "! {\""+buf2+"\"}@{"+EMBED_LABEL+", div}!" + q.substring(index1+index2);
+			if(!buf2.contains("$$")){
+				//<$  $>
+				//buf2 = buf2.replace("\"", "\\\"");	//TODO check
+				q = q.substring(0,index1) + "! {\""+buf2+"\"}@{"+EMBED_LABEL+", div}!" + q.substring(index1+index2);
+			}else{
+				//<$  $$  $$  $>
+				// <h1> $$'aaa h1'$$ </h1> 
+				//' <a href= '||''bb''||' > '||''aa''||' </a> '
+				buf2 = buf2.replace("\"", "'").replace("'", "''");	//TODO check
+				//Log.e(buf2);
+				String c = buf2, c2 = "", x = "";
+				boolean d = false;
+				for(int k=0; k<c.length(); k++){
+					if(k>0){
+						if(c.charAt(k-1)=='$' && c.charAt(k)=='$'){
+							if(!d){
+								x = c.substring(k+1);
+								if(x.trim().startsWith("''")){
+									//not attribute process
+									x = x.substring(x.indexOf("'")+1);
+								}
+								c2 = c.substring(0,k-1)+"'||"+x;
+								c = c2;
+								d = true;
+							}else{
+								x = c.substring(0,k-1);
+								if(x.trim().endsWith("''")){
+									//not attribute process
+									x = x.substring(0, x.lastIndexOf("'"));
+								}
+								c2 = x+"||'"+c.substring(k+1);
+								c = c2;
+								d = false;
+							}
+						}
+					}
+				}
+				buf2 = c;
+				q = q.substring(0,index1) + " {'"+buf2+"'}@{"+EMBED_LABEL+", div}!" + q.substring(index1+index2);
+			}
 			//Log.e(buf2);
 		}
 		//Log.e("q="+q);
-		
-//		//goto 20130915-2  "<$  $>"
-//		if (line.contains("<$")){
-//			String line1 = line.substring(0, line.indexOf("<$"));
-//			String buf = "";
-//			while (!line.contains("$>")){
-//				line = in.readLine();
-//				buf += line+"\n";
-//			}
-//			buf = buf.substring(0,buf.lastIndexOf("$>"));	//substring last '?>'
-//			textString.add(textNum, buf);
-//			line = line1 + line.substring(line.indexOf("$>") + 2);
-//			Log.e(textString.get(textNum));
-//			textNum++;
-//		}
-//		try{
-//			if (line.startsWith(IMPORT)){
-//				while (line.startsWith(IMPORT)){
-//					importString.add(line);
-//					line = in.readLine();
-//				}
-//			}
-//		}catch (Exception e) {	}
-//		return line;
 		return q;
 	}
 	
-	public static boolean embed = false;
 	
 	public static StringBuffer preProcess(StringBuffer code, DecorateList decos) {
 		if(isEmbed(decos)){
