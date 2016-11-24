7d3eb8f32b1a2d661887a8b9f1aa54d6261bcad4
kotani922 <kotani@db.ics.keio.ac.jp>
2016-11-24 17:13:01 +0900

    added medium:VR

---
+++ supersql/codegenerator/VR/VRC2.java
@@ -0,0 +1,266 @@
+package supersql.codegenerator.VR;
+
+import org.stringtemplate.v4.compiler.STParser.ifstat_return;
+
+import com.ibm.db2.jcc.am.t;
+
+import supersql.codegenerator.Connector;
+import supersql.codegenerator.ITFE;
+import supersql.codegenerator.Manager;
+import supersql.common.GlobalEnv;
+import supersql.common.Log;
+import supersql.extendclass.ExtList;
+
+public class VRC2 extends Connector {
+
+	private VREnv htmlEnv;
+	private VREnv htmlEnv2;
+
+	// 鐃緒申鐃藷ストラク鐃緒申
+	public VRC2(Manager manager, VREnv henv, VREnv henv2) {
+		this.htmlEnv = henv;
+		this.htmlEnv2 = henv2;
+	}
+
+	@Override
+	public String getSymbol() {
+		return "HTMLC2";
+	}
+
+	// C2鐃緒申work鐃潤ソ鐃獣ワ申
+	@Override
+	public String work(ExtList data_info) {
+		Log.out("------- C2 -------");
+		Log.out("tfes.contain_itemnum=" + tfes.contain_itemnum());
+		Log.out("tfessize=" + tfes.size());
+		Log.out("countconnetitem=" + countconnectitem());
+
+		this.setDataList(data_info);
+
+		if (decos.containsKey("form")) {
+			htmlEnv2.code
+					.append("<form" + VREnv.getFormNumber() + "start />");
+			if (decos.getStr("form").toLowerCase().equals("search")) {
+				VREnv.setSearch(true);
+			}
+		}
+
+		if (decos.containsKey("insert")) {
+			VREnv.setIDU("insert");
+		}
+		if (decos.containsKey("update")) {
+			VREnv.setIDU("update");
+		}
+		if (decos.containsKey("delete")) {
+			VREnv.setIDU("delete");
+		}
+
+		// tk
+		// start////////////////////////////////////////////////////////////////
+		htmlEnv.append_css_def_td(VREnv.getClassID(this), this.decos);
+
+//		if (!GlobalEnv.isOpt()) {
+////			htmlEnv.code
+////					.append("<TABLE cellSpacing=\"0\" cellPadding=\"0\" border=\"");
+//			htmlEnv.code.append(htmlEnv.tableBorder + "\" ");
+//			htmlEnv.code.append(htmlEnv.getOutlineMode());
+//			if (htmlEnv.writtenClassId.contains(VREnv.getClassID(this))) {
+//				htmlEnv.code.append(" class=\"");
+//				htmlEnv.code.append(VREnv.getClassID(this));
+//			}
+//
+//			if (decos.containsKey("class")) {
+//				if (!htmlEnv.writtenClassId.contains(VREnv.getClassID(this))) {
+//					htmlEnv.code.append(" class=\"");
+//				} else {
+//					htmlEnv.code.append(" ");
+//				}
+//				htmlEnv.code.append(decos.getStr("class") + "\" ");
+//			} else if (htmlEnv.writtenClassId
+//					.contains(VREnv.getClassID(this))) {
+//				htmlEnv.code.append("\" ");
+//			}
+//			htmlEnv.code.append(">");
+//		}
+		if (GlobalEnv.isOpt()) {
+			htmlEnv2.code.append("<tfe type=\"connect\" dimension=\"2\"");
+			if (decos.containsKey("tablealign"))
+				htmlEnv2.code.append(" align=\"" + decos.getStr("tablealign")
+						+ "\"");
+			if (decos.containsKey("tablevalign"))
+				htmlEnv2.code.append(" valign=\"" + decos.getStr("tablevalign")
+						+ "\"");
+			if (decos.containsKey("height"))
+				htmlEnv2.code.append(" height=\"" + decos.getStr("height")
+						+ "\"");
+			if (decos.containsKey("tabletype")) {
+				htmlEnv2.code.append(" tabletype=\""
+						+ decos.getStr("tabletype") + "\"");
+				if (decos.containsKey("cellspacing")) {
+					htmlEnv2.code.append(" cellspacing=\""
+							+ decos.getStr("cellspacing") + "\"");
+				}
+				if (decos.containsKey("cellpadding")) {
+					htmlEnv2.code.append(" cellpadding=\""
+							+ decos.getStr("cellpadding") + "\"");
+				}
+				if (decos.containsKey("border")) {
+					htmlEnv2.code.append(" border=\""
+							+ decos.getStr("border").replace("\"", "") + "\"");
+				}
+
+				if (decos.containsKey("tableborder")) {
+					htmlEnv2.code.append(" tableborder=\""
+							+ decos.getStr("tableborder").replace("\"", "")
+							+ "\"");
+				}
+			} else {
+				if (decos.containsKey("border")) {
+					htmlEnv2.code.append(" border=\""
+							+ decos.getStr("border").replace("\"", "") + "\"");
+				} else {
+					htmlEnv2.code.append(" border=\""
+							+ htmlEnv.tableBorder.replace("\"", "") + "\"");
+				}
+				if (decos.containsKey("tableborder")) {
+					htmlEnv2.code.append(" tableborder=\""
+							+ decos.getStr("tableborder").replace("\"", "")
+							+ "\"");
+				}
+			}
+			if (htmlEnv.writtenClassId.contains(VREnv.getClassID(this))) {
+				htmlEnv2.code.append(" class=\"");
+				htmlEnv2.code.append(VREnv.getClassID(this));
+			}
+
+			if (decos.containsKey("class")) {
+				if (!htmlEnv.writtenClassId.contains(VREnv.getClassID(this))) {
+					htmlEnv2.code.append(" class=\"");
+				} else {
+					htmlEnv2.code.append(" ");
+				}
+				htmlEnv2.code.append(decos.getStr("class"));
+			} else if (htmlEnv.writtenClassId
+					.contains(VREnv.getClassID(this))) {
+				htmlEnv2.code.append("\" ");
+			}
+
+			if (decos.containsKey("form")) {
+				htmlEnv2.code.append(" form=\"" + VREnv.getFormNumber()
+						+ "\" ");
+			}
+
+			htmlEnv2.code.append(">");
+		}
+		/*
+		 * if(decos.containsKey("outborder"))
+		 * html_env.code.append(" noborder ");
+		 */
+
+		// html_env2.code.append(">");
+
+		// System.out.println(html_env.code);
+
+		// tk
+		// end//////////////////////////////////////////////////////////////////
+
+		// Log.out("<TABLE class=\""+HTMLEnv.getClassID(this) + "\">");
+
+		int i = 0;
+
+		if (decos.containsKey("form")) {
+			htmlEnv.code.append(VRFunction.createForm(decos));
+			VREnv.setFormItemFlg(true, null);
+		}
+
+		System.out.println("<C2front>");
+		while (this.hasMoreItems()) {
+			
+			if(htmlEnv.gLevel == 1){//////// kotani 16/10/04
+				 
+			}
+			ITFE tfe = tfes.get(i);
+			if(VRAttribute.genre.equals("")){//////////////////////////////////////////////////////////// kotani 16/10/04
+				if(htmlEnv.gLevel == 0){
+					//htmlEnv.code.append(VREnv.getClassID(tfe));
+					if(VRAttribute.groupcount >= 1){
+						htmlEnv.code.append("<group>\n");
+					}
+					VRAttribute.groupcount++;
+					//System.out.println("groupcount2");
+				}else{
+	//				htmlEnv.code.append("<TD class=\"" + VREnv.getClassID(tfe) + " nest\">\n");
+					htmlEnv.code.append("<category" + VREnv.getClassID(tfe) + " > \n");
+				}
+			}else{
+				//htmlEnv.code.append("<TD class=\"" + VREnv.getClassID(tfe) + " nest\" name = \"" +VRAttribute.genre+ "\"> \n");
+				htmlEnv.code.append("<category " + VREnv.getClassID(tfe) + " name = \"" +VRAttribute.genre+ "\"> \n");
+//				VRAttribute.genrearray2.add("\"" + VRAttribute.genre + "\"");
+//				if(VRAttribute.genrecount == 0){
+//					VRAttribute.genrearray22.add(0);
+//				}
+//				VRAttribute.genrecount++;
+				//System.out.println(VRAttribute.genrecount);
+			}
+			String classid = VREnv.getClassID(tfe);
+			// Log.out("<TR><TD class=\"nest "
+			// + HTMLEnv.getClassID(tfe) + " nest\"> decos:" + decos);
+
+			this.worknextItem();
+
+//			if (htmlEnv.notWrittenClassId.contains(classid)) {
+//				htmlEnv.code.delete(htmlEnv.code.indexOf(classid),
+//						htmlEnv.code.indexOf(classid) + classid.length() + 1);
+//			}
+
+			//htmlEnv.code.append("</TD>\n");////</TR>消した
+			Log.out("</TD>");////</TR>消した
+
+			i++;
+
+		}
+
+		if(VRAttribute.gjudge == 0){
+			if(VRAttribute.billnum >= 2){
+				for(int k=0;k<VRAttribute.billnum-1;k++){
+					VRAttribute.cjoinarray.add("C2");
+				}
+				VRAttribute.billnum = 0;
+			}else{
+				VRAttribute.cjoinarray.add("C2");
+			}
+		}
+		System.out.println("</C2back>");
+		
+		htmlEnv2.code.append("</tfe>");
+		// Log.out("</TABLE>");
+		if (decos.containsKey("form")) {
+			htmlEnv2.code.append("<form" + VREnv.getFormNumber() + "end />");
+			htmlEnv.code.append(VREnv.exFormNameCreate());
+			htmlEnv.code.append("</form>");
+			VREnv.setFormItemFlg(false, null);
+			VREnv.incrementFormNumber();
+			if (decos.getStr("form").toLowerCase().equals("search"))
+				VREnv.setSearch(false);
+		}
+
+//////////////ここら辺のcategory正直使ってない
+//		htmlEnv.code.append("</category>\n"); //htmlEnv.code.append("</TABLE>\n")から変更
+//		htmlEnv.code.append("</category>\n");//20160919 kotani add
+		
+		
+//		if(!VRAttribute.gjudge){
+////		System.out.println("c2_level="+htmlEnv.gLevel2);
+//		//if(htmlEnv.gLevel2 == 0){
+//			//VRAttribute.cjoinflag = 2;
+//			VRAttribute.cjoinarray.add("C2");
+//		//}
+//		}
+
+		Log.out("TFEId = " + VREnv.getClassID(this));
+		// html_env.append_css_def_td(HTMLEnv.getClassID(this), this.decos);
+		return null;
+
+	}
+
+}
---
+++ supersql/codegenerator/VR/VRManager.java
@@ -0,0 +1,548 @@
+package supersql.codegenerator.VR;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Vector;
+
+import com.mysql.jdbc.StatementInterceptorV2;
+
+import supersql.codegenerator.CodeGenerator;
+import supersql.codegenerator.ITFE;
+import supersql.codegenerator.Jscss;
+import supersql.codegenerator.Manager;
+import supersql.common.GlobalEnv;
+import supersql.common.Log;
+import supersql.common.Utils;
+import supersql.dataconstructor.DataConstructor;
+import supersql.extendclass.ExtList;
+
+public class VRManager extends Manager {
+
+	private VREnv htmlEnv;
+	private VREnv htmlEnv2;
+	public static int i=0;
+
+	public VRManager(VREnv henv, VREnv henv2) {
+		this.htmlEnv = henv;
+		this.htmlEnv2 = henv2;
+	}
+
+	private void connectOutdir(String outdir, String outfile) {
+		// added by goto 20120627 start
+		String fileDir = new File(htmlEnv.outFile).getAbsoluteFile()
+				.getParent();
+		if (fileDir.length() < htmlEnv.outFile.length()
+				&& fileDir
+				.equals(htmlEnv.outFile.substring(0, fileDir.length())))
+			htmlEnv.outFile = htmlEnv.outFile.substring(fileDir.length() + 1); // 鐃緒申鐃出パワ申鐃春ワ申鐃緒申鐃緒申名
+		// added by goto 20120627 end
+
+		String tmpqueryfile = new String();
+		if (htmlEnv.outFile.indexOf("/") > 0) {
+			if (outfile != null) {
+				if (htmlEnv.outFile.startsWith(".")
+						|| htmlEnv.outFile.startsWith("/")) {
+					tmpqueryfile = htmlEnv.outFile.substring(htmlEnv.outFile
+							.indexOf("/") + 1);
+				}
+			} else {
+				tmpqueryfile = htmlEnv.outFile.substring(htmlEnv.outFile
+						.lastIndexOf("/") + 1);
+			}
+		} else {
+			tmpqueryfile = htmlEnv.outFile;
+		}
+		if (!outdir.endsWith("/")) {
+			outdir = outdir.concat("/");
+		}
+		htmlEnv.outFile = outdir.concat(tmpqueryfile);
+	}
+
+	private String getOutfile(String outfile) {
+		String out = new String();
+		if (outfile.indexOf(".xml") > 0) {
+			out = outfile.substring(0, outfile.indexOf(".xml"));
+		} else {
+			out = outfile;
+		}
+		return out;
+	}
+
+	private int indexOf(String string) {
+		// TODO 鐃緒申動鐃緒申鐃緒申鐃緒申鐃曙た鐃潤ソ鐃獣ド¥申鐃緒申鐃緒申鐃緒申
+		return 0;
+	}
+
+	private int lastIndexOf(String string) {
+		// TODO 鐃緒申動鐃緒申鐃緒申鐃緒申鐃曙た鐃潤ソ鐃獣ド¥申鐃緒申鐃緒申鐃緒申
+		return 0;
+	}
+
+	protected void getOutfilename() {
+		String file = GlobalEnv.getfilename();
+		String outdir = GlobalEnv.getoutdirectory();
+		String outfile = GlobalEnv.getoutfilename();
+		htmlEnv.outDir = outdir;
+
+		/*
+		 * 鐃緒申鐃熟フワ申鐃緒申?(outfilename)鐃緒申鐃緒申鐃所さ?鐃銃わ申???
+		 * html_env.outfile鐃緒申globalenv.outfilename鐃祝わ申?
+		 * 鐃緒申?鐃淑鰹申鐃塾とわ申鐃熟ワ申鐃緒申?鐃春ワ申鐃緒申?鐃緒申名鐃緒申(filename)鐃祝わ申?
+		 */
+		if (GlobalEnv.getQuery() != null) {
+			htmlEnv.outFile = "./fromquery";
+
+		} else if (outfile == null) {
+			if (file.toLowerCase().indexOf(".sql") > 0) {
+				htmlEnv.outFile = file.substring(0,
+						file.toLowerCase().indexOf(".sql"));
+			} else if (file.toLowerCase().indexOf(".ssql") > 0) {
+				htmlEnv.outFile = file.substring(0,
+						file.toLowerCase().indexOf(".ssql"));
+			}
+		} else {
+			htmlEnv.outFile = getOutfile(outfile);
+		}
+
+		if (htmlEnv.outFile.indexOf("/") > 0) {
+			htmlEnv.linkOutFile = htmlEnv.outFile.substring(htmlEnv.outFile
+					.lastIndexOf("/") + 1);
+		} else {
+			htmlEnv.linkOutFile = htmlEnv.outFile;
+		}
+		/*
+		 * //tk start if(html_env.outfile.lastIndexOf("\\") != -1) {
+		 * html_env.outfile =
+		 * html_env.outfile.substring(html_env.outfile.lastIndexOf("\\"));
+		 * Log.out("outfile log:"+html_env.outfile); } //tk end
+		 */
+		/*
+		 * 鐃緒申鐃緒申鐃緒申妊鐃����?(outdirectory)�����ꤵ?�Ƥ�???
+		 * outdirectory��filename��Ĥʤ�����Τ�file�Ȥ�?
+		 */
+
+		if (outdir != null) {
+			connectOutdir(outdir, outfile);
+		}
+	}
+
+	@Override
+	public void finish() {
+
+	}
+
+	@Override
+	public void generateCode(ITFE tfe_info, ExtList data_info) {
+
+		VREnv.initAllFormFlg();
+
+		htmlEnv.countFile = 0;
+		htmlEnv.code = new StringBuffer();
+		htmlEnv.css = new StringBuffer();
+		htmlEnv.header = new StringBuffer();
+		htmlEnv.footer = new StringBuffer();
+		htmlEnv.foreachFlag = GlobalEnv.getForeachFlag();
+		htmlEnv.writtenClassId = new Vector();
+		htmlEnv.notWrittenClassId = new Vector();
+		htmlEnv2.countFile = 0;
+		htmlEnv2.code = new StringBuffer();
+		htmlEnv2.css = new StringBuffer();
+		htmlEnv2.header = new StringBuffer();
+		htmlEnv2.footer = new StringBuffer();
+		htmlEnv2.foreachFlag = GlobalEnv.getForeachFlag();
+		htmlEnv2.writtenClassId = new Vector<String>();
+		VREnv localenv = new VREnv();
+
+		/*** start oka ***/
+
+		// ���Ϥ�?�ե���?̾����?
+		getOutfilename();
+
+		Log.out("[HTMLManager:generateCode]");
+
+		// ?�ֳ�¦��G3��??
+		if (tfe_info instanceof VRG3) {
+			//tfe_info.work(data_info);
+//			return;
+		}
+
+		// ?�ֳ�¦��G3�Ǥʤ�??]
+		htmlEnv.fileName = htmlEnv.outFile + ".xml";
+		htmlEnv2.fileName = htmlEnv.outFile + ".xml";
+
+		htmlEnv.setOutlineMode();
+		if (data_info.size() == 0
+				// added by goto 20130306 "FROM�ʤ��������к� 3/3"
+				&& !DataConstructor.SQL_string
+				.equals("SELECT DISTINCT  FROM ;")) {
+			Log.out("no data");
+			htmlEnv.code.append("<div class=\"nodata\" >");
+			htmlEnv.code.append("NO DATA FOUND");
+			//htmlEnv.code.append("</div>");
+		} else
+			tfe_info.work(data_info);
+		VREnv.cs_code.append("9 "+tfe_info+"\n");
+		
+		//htmlEnv.getHeader();
+		htmlEnv.getFooter();
+		//htmlEnv2.header.append("<?xml version=\"1.0\" encoding=\""+ Utils.getEncode() + "\"?><SSQL>");
+		htmlEnv2.header.append("<?xml version=\"1.0\" ?>");//上の行をこれに変更
+		//htmlEnv2.footer.append("</SSQL>");
+		try {
+
+			if(CodeGenerator.getMedia().equalsIgnoreCase("vr")){
+/////////////////////////////xmlcreateに使った
+				if (!GlobalEnv.isOpt()) {
+					// changed by goto 20120715 start
+					// PrintWriter pw = new PrintWriter(new BufferedWriter(new
+					// FileWriter(
+					// html_env.filename)));
+					PrintWriter pw;
+					if (htmlEnv.charset != null) {
+						pw = new PrintWriter(new BufferedWriter(
+								new OutputStreamWriter(new FileOutputStream(
+										htmlEnv.fileName), htmlEnv.charset)));
+						Log.info("File encoding: " + htmlEnv.charset);
+					} else
+						pw = new PrintWriter(new BufferedWriter(new FileWriter(
+								htmlEnv.fileName)));
+					// Log.info("File encoding: "+((html_env.charset!=null)?
+					// html_env.charset : "UTF-8"));
+					// changed by goto 20120715 end
+
+					if (GlobalEnv.cssout() == null)
+						pw.println(htmlEnv.header);
+					pw.println(htmlEnv.code);
+					pw.println(htmlEnv.footer);
+					pw.close();
+					
+					/////////////////////////////
+					htmlEnv.cs_code.append("heeeeeeweeee");
+					if (htmlEnv.charset != null) {
+						pw = new PrintWriter(new BufferedWriter(
+								new OutputStreamWriter(new FileOutputStream(
+										htmlEnv.fileName+".cs"), htmlEnv.charset)));
+						Log.info("File encoding: " + htmlEnv.charset);
+					} else
+						pw = new PrintWriter(new BufferedWriter(new FileWriter(
+								htmlEnv.fileName+".cs")));
+					// Log.info("File encoding: "+((html_env.charset!=null)?
+					// html_env.charset : "UTF-8"));
+					// changed by goto 20120715 end
+
+					if (GlobalEnv.cssout() == null)
+					pw.println(htmlEnv.cs_code);
+					pw.close();////////////////////////////
+					
+				}
+				/////////////////////////////xmlcreateに使った
+				// xml
+				if (GlobalEnv.isOpt()) {
+
+					/*
+					 * int i=0; while(html_env2.code.indexOf("&",i) != -1){ i =
+					 * html_env2.code.indexOf("&",i); html_env2.code =
+					 * html_env2.code.replace(i,i+1, "&amp;"); i++; }
+					 */
+
+					htmlEnv2.fileName = htmlEnv.outFile + ".xml";
+					PrintWriter pw2 = new PrintWriter(new BufferedWriter(
+							new FileWriter(htmlEnv2.fileName)));
+					if (GlobalEnv.cssout() == null)
+						pw2.println(htmlEnv2.header);
+					pw2.println(htmlEnv2.code);
+					pw2.println(htmlEnv2.footer);
+					pw2.close();
+					VRoptimizer xml = new VRoptimizer();
+					String xml_str = xml.generateHtml(htmlEnv2.fileName);
+					PrintWriter pw = new PrintWriter(new BufferedWriter(
+							new FileWriter(htmlEnv.fileName)));
+					pw.println(htmlEnv.header);
+					pw.println(xml_str);
+					// StringBuffer footer = new
+					// StringBuffer("</div></body></html>");
+					pw.println(htmlEnv.footer);
+					pw.close();
+				}
+
+				if (GlobalEnv.cssout() != null) {
+					PrintWriter pw3 = new PrintWriter(new BufferedWriter(
+							new FileWriter(GlobalEnv.cssout())));
+					pw3.println(htmlEnv.header);
+					pw3.close();
+				}
+				// add 20141204 masato for ehtml
+			} else {
+				Log.ehtmlInfo("=-=-=-=");
+				Log.ehtmlInfo(htmlEnv.header);
+				Log.ehtmlInfo(htmlEnv.code);
+				Log.ehtmlInfo(htmlEnv.footer);
+			}
+
+			VREnv.initAllFormFlg();
+			//Jscss.process();	//goto 20141209
+			//VRfilecopy.process(); 
+			VRfilecreate.process(htmlEnv.outFile); 
+		} catch (FileNotFoundException fe) {
+			fe.printStackTrace();
+			Log.err("Error: specified outdirectory \""
+					+ htmlEnv.outDir + "\" is not found to write "
+					+ htmlEnv.fileName);
+			GlobalEnv.addErr("Error: specified outdirectory \""
+					+ htmlEnv.outDir + "\" is not found to write "
+					+ htmlEnv.fileName);
+			// comment out by chie
+			// System.exit(-1);
+		} catch (IOException e) {
+			System.err
+			.println("Error[HTMLManager]: File IO Error in HTMLManager");
+			e.printStackTrace();
+			GlobalEnv
+			.addErr("Error[HTMLManager]: File IO Error in HTMLManager");
+			// comment out by chie
+			// System.exit(-1);
+		}
+
+	}
+
+	// tk
+	// start///////////////////////////////////////////////////////////////////////
+	@Override
+	public StringBuffer generateCode2(ITFE tfe_info, ExtList data_info) {
+		VREnv.initAllFormFlg();
+
+		htmlEnv.countFile = 0;
+		htmlEnv.code = new StringBuffer();
+		htmlEnv.css = new StringBuffer();
+		htmlEnv.header = new StringBuffer();
+		htmlEnv.footer = new StringBuffer();
+		htmlEnv.foreachFlag = GlobalEnv.getForeachFlag();
+		htmlEnv.writtenClassId = new Vector();
+		htmlEnv.embedFlag = true;
+
+		htmlEnv2.countFile = 0;
+		htmlEnv2.code = new StringBuffer();
+		htmlEnv2.css = new StringBuffer();
+		htmlEnv2.header = new StringBuffer();
+		htmlEnv2.footer = new StringBuffer();
+		String xml_str = null;
+		StringBuffer returncode = new StringBuffer();
+		// ���Ϥ�?�ե���?̾����?
+		getOutfilename();
+
+		Log.out("[HTMLManager:generateCode2]");
+
+		// ?�ֳ�¦��G3��??
+		if (tfe_info instanceof VRG3) {
+			tfe_info.work(data_info);
+			VREnv.cs_code.append("10 "+tfe_info+"\n");
+			return htmlEnv.code;
+		}
+		// ?�ֳ�¦��G3�Ǥʤ�??
+		htmlEnv.setOutlineMode();
+		tfe_info.work(data_info);
+		VREnv.cs_code.append("100 "+tfe_info+"\n");
+
+		htmlEnv2.header.append("<?xml version=\"1.0\" encoding=\"Shift_JIS\"?><SSQL>");
+		htmlEnv2.footer.append("</SSQL>");
+
+		if (GlobalEnv.isOpt()) {
+			int i = 0;
+			while (htmlEnv2.code.indexOf("&", i) != -1) {
+				i = htmlEnv2.code.indexOf("&", i);
+				htmlEnv2.code = htmlEnv2.code.replace(i, i + 1, "&amp;");
+				i++;
+			}
+			StringBuffer xml_string = new StringBuffer();
+			xml_string.append(htmlEnv2.header);
+			xml_string.append(htmlEnv2.code);
+			xml_string.append(htmlEnv2.footer);
+			VRoptimizer xml = new VRoptimizer();
+			// System.out.println(xml_string); //commented out by goto 20120620
+			xml_str = xml.generateHtml(xml_string);
+			returncode.append(xml_str);
+		}
+		htmlEnv.embedFlag = false;
+
+		if (htmlEnv.script.length() >= 5) {
+			StringBuffer result = new StringBuffer();
+
+			result.append(htmlEnv.script);
+			result.append("<end of script>\n");
+			result.append(htmlEnv.code);
+
+			return result;
+		} else {
+			if (GlobalEnv.isOpt())
+				return returncode;
+			else
+				return htmlEnv.code;
+
+		}
+	}
+
+	@Override
+	public StringBuffer generateCode3(ITFE tfe_info, ExtList data_info) {
+		VREnv.initAllFormFlg();
+
+		htmlEnv.countFile = 0;
+		htmlEnv.code = new StringBuffer();
+		htmlEnv.css = new StringBuffer();
+		htmlEnv.header = new StringBuffer();
+		htmlEnv.footer = new StringBuffer();
+		htmlEnv.foreachFlag = GlobalEnv.getForeachFlag();
+		htmlEnv.writtenClassId = new Vector();
+		htmlEnv.embedFlag = true;
+		// ���Ϥ�?�ե���?̾����?
+		getOutfilename();
+
+		Log.out("[HTMLManager:generateCode]");
+
+		// ?�ֳ�¦��G3��??
+		if (tfe_info instanceof VRG3) {
+			tfe_info.work(data_info);
+			VREnv.cs_code.append("101 "+tfe_info+"\n");
+			return htmlEnv.code;
+		}
+		// ?�ֳ�¦��G3�Ǥʤ�??
+
+		htmlEnv.setOutlineMode();
+		tfe_info.work(data_info);
+		VREnv.cs_code.append("102 "+tfe_info+"\n");
+		// html_env.getCSS();
+		htmlEnv.embedFlag = false;
+		Log.out("header : " + htmlEnv.header);
+		return htmlEnv.css;
+	}
+
+	@Override
+	public StringBuffer generateCode4(ITFE tfe_info, ExtList data_info) {
+		VREnv.initAllFormFlg();
+		htmlEnv.countFile = 0;
+		htmlEnv.code = new StringBuffer();
+		htmlEnv.css = new StringBuffer();
+		htmlEnv.header = new StringBuffer();
+		htmlEnv.footer = new StringBuffer();
+		htmlEnv.foreachFlag = GlobalEnv.getForeachFlag();
+		htmlEnv.writtenClassId = new Vector();
+
+		htmlEnv2.countFile = 0;
+		htmlEnv2.code = new StringBuffer();
+		htmlEnv2.css = new StringBuffer();
+		htmlEnv2.header = new StringBuffer();
+		htmlEnv2.footer = new StringBuffer();
+		htmlEnv2.foreachFlag = GlobalEnv.getForeachFlag();
+		htmlEnv2.writtenClassId = new Vector<String>();
+
+		VREnv localenv = new VREnv();
+
+		// ���Ϥ�?�ե���?̾����?
+		getOutfilename();
+
+		Log.out("[HTMLManager:generateCode]");
+
+		// ?�ֳ�¦��G3�Ǥʤ�??
+		htmlEnv.fileName = htmlEnv.outFile + ".xml";
+		htmlEnv2.fileName = htmlEnv.outFile + ".xml";
+
+		htmlEnv.setOutlineMode();
+		tfe_info.work(data_info);
+		VREnv.cs_code.append("103 "+tfe_info+"\n");
+
+		//htmlEnv.getHeader();
+		htmlEnv.getFooter();
+		htmlEnv.embedFlag = false;
+		Log.out("header : " + htmlEnv.header);
+
+		StringBuffer headfoot = new StringBuffer(htmlEnv.header
+				+ " ###split### " + htmlEnv.footer);
+
+		return headfoot;
+	}
+
+	@Override
+	public StringBuffer generateCodeNotuple(ITFE tfe_info) {
+		Log.out("no data found");
+		htmlEnv.code = new StringBuffer();
+		htmlEnv.code.append("<div class=\"nodata\" >");
+		htmlEnv.code.append("NO DATA FOUND");
+		//htmlEnv.code.append("</div>");
+
+		return htmlEnv.code;
+	}
+
+	@Override
+	public StringBuffer generateCssfile(ITFE tfe_info, ExtList data_info) {
+
+		htmlEnv.countFile = 0;
+		htmlEnv.code = new StringBuffer();
+		htmlEnv.css = new StringBuffer();
+		htmlEnv.header = new StringBuffer();
+		htmlEnv.footer = new StringBuffer();
+		htmlEnv.foreachFlag = GlobalEnv.getForeachFlag();
+		htmlEnv.writtenClassId = new Vector();
+		htmlEnv.embedFlag = true;
+		// ���Ϥ�?�ե���?̾����?
+		getOutfilename();
+
+		Log.out("[HTMLManager:generateCode]");
+
+		htmlEnv.setOutlineMode();
+		tfe_info.work(data_info);
+		VREnv.cs_code.append("104 "+tfe_info+"\n");
+		htmlEnv.embedFlag = false;
+		Log.out("header : " + htmlEnv.header);
+		return htmlEnv.cssFile;
+	}
+	
+//	public void xmlcreate(){
+//		htmlEnv.fileName = htmlEnv.outFile +i + ".xml";
+//			
+//		if(CodeGenerator.getMedia().equalsIgnoreCase("vr")){
+//
+//			if (!GlobalEnv.isOpt()) {
+//				// changed by goto 20120715 start
+//				// PrintWriter pw = new PrintWriter(new BufferedWriter(new
+//				// FileWriter(
+//				// html_env.filename)));
+//				try {
+//
+//				PrintWriter pw;
+//				if (htmlEnv.charset != null) {
+//					pw = new PrintWriter(new BufferedWriter(
+//							new OutputStreamWriter(new FileOutputStream(
+//									htmlEnv.fileName), htmlEnv.charset)));
+//					Log.info("File encoding: " + htmlEnv.charset);
+//				} else
+//					pw = new PrintWriter(new BufferedWriter(new FileWriter(
+//							htmlEnv.fileName)));
+//				
+//				if (GlobalEnv.cssout() == null)
+//					pw.println(htmlEnv.header);
+//					pw.println(htmlEnv.code);
+//					pw.println(htmlEnv.footer);
+//					pw.close();
+//									
+//				} catch (Exception e) {
+//					// TODO: handle exception
+//				}
+//				// Log.info("File encoding: "+((html_env.charset!=null)?
+//				// html_env.charset : "UTF-8"));
+//				// changed by goto 20120715 end
+//
+//				htmlEnv.code = new StringBuffer();
+//				
+//			}
+//		}
+//	}
+	
+	
+	// tk
+	// end///////////////////////////////////////////////////////////////////////////////
+}
